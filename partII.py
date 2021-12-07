31793d9d9fdea033204ca3ed14f92d7c28b0b9d0
Waerness <tarald.waerness@bearingpoint.com>
2021-12-07 19:14:38 +0100

    Oscar er en loek

--- partII.py
+++ partII.py
@@ -8,7 +8,7 @@ import json
 host_server = os.environ.get('HOST_SERVER', 'pssql-fastapihackatahon01.postgres.database.azure.com')
 db_name = os.environ.get('DB_NAME', 'postgres')
 db_username = os.environ.get('DB_USERNAME', 'pssqlfastapihackathonadmin')
-db_password = os.environ.get('DB_PASSWORD', '')
+db_password = os.environ.get('DB_PASSWORD', '+@VYFn#R*R32%?77')
 ssl_mode = os.environ.get('SSL_MODE','require')
 
 app = FastAPI()
@@ -66,6 +66,86 @@ async def get_customer_highest_prediction(customer_id:int):
     query = "Select * from fastapihackathon.CustomerScore where customer_id = %s ORDER BY score DESC LIMIT 1"
 
     records = cur.execute(query,(customer_id,))
+    
+    records = cur.fetchall()
+     
+    record = [dict((cur.description[i][0],value) for i, value in enumerate(row)) for row in records]
+    return record
+
+@app.get("/v1/Customer/GetUpgradeScore/{customer_id}")
+async def get_upgrade_score(customer_id:int):
+
+    conn = psycopg2.connect(
+    host=host_server,
+    database=db_name,
+    user=db_username,
+    password=db_password)
+
+    cur = conn.cursor()
+
+    query = "Select * from fastapihackathon.CustomerScore where customer_id = %s and model_name = 'upgrade_subscription_30d'"
+
+    records = cur.execute(query,(customer_id,))
+    
+    records = cur.fetchall()
+     
+    record = [dict((cur.description[i][0],value) for i, value in enumerate(row)) for row in records]
+    return record
+
+@app.get("/v1/Customer/GetDowngradeScore/{customer_id}")
+async def get_downgrade_score(customer_id:int):
+
+    conn = psycopg2.connect(
+    host=host_server,
+    database=db_name,
+    user=db_username,
+    password=db_password)
+
+    cur = conn.cursor()
+
+    query = "Select * from fastapihackathon.CustomerScore where customer_id = %s and model_name = 'downgrade_subscription_30d'"
+
+    records = cur.execute(query,(customer_id,))
+    
+    records = cur.fetchall()
+     
+    record = [dict((cur.description[i][0],value) for i, value in enumerate(row)) for row in records]
+    return record
+
+@app.get("/v1/Customer/GetAllScores/{customer_id}")
+async def get_all_scores(customer_id:int):
+
+    conn = psycopg2.connect(
+    host=host_server,
+    database=db_name,
+    user=db_username,
+    password=db_password)
+
+    cur = conn.cursor()
+
+    query = "Select * from fastapihackathon.CustomerScore where customer_id = %s"
+
+    records = cur.execute(query,(customer_id,))
+    
+    records = cur.fetchall()
+     
+    record = [dict((cur.description[i][0],value) for i, value in enumerate(row)) for row in records]
+    return record
+
+@app.get("/v1/Customer/GetTopXCustomers/{X}")
+async def get_top_x_customers(X:int):
+
+    conn = psycopg2.connect(
+    host=host_server,
+    database=db_name,
+    user=db_username,
+    password=db_password)
+
+    cur = conn.cursor()
+
+    query = "Select * from fastapihackathon.CustomerScore ORDER BY score DESC LIMIT %s"
+
+    records = cur.execute(query,(X,))
     
     records = cur.fetchall()
      
